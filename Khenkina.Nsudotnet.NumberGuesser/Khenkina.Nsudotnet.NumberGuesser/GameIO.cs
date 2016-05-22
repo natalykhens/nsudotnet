1beef98594bdd398154120e2cded5ff196e0c4b2
Natalia Khenkina <natalykhens@gmail.com>
2016-05-23 00:55:48 +0600

    commit
    commit

---
+++ Khenkina.Nsudotnet.NumberGuesser/Khenkina.Nsudotnet.NumberGuesser/GameIO.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Khenkina.Nsudotnet.NumberGuesser
+{
+    class GameIO
+    {
+        private static readonly string[] OATH =
+        {
+         
+                    "BYSTREE, %NAME!",
+                    " ESCHO BYSTREE, %NAME?",
+                    "%NAME, TORMOZ!"
+        };
+
+        public static void WriteResult(int attempts, TimeSpan playedTime)
+        {
+            Console.WriteLine("DONE {0} ITER ( {1}). PLAY MORE?",attempts,playedTime);
+            // Console.WriteLine($"Хорош, отгадал за {attempts} попыток (за {playedTime}). Ещё будешь играть?");
+        }
+
+        public static void WriteNumberIsMore()
+        {
+            Console.WriteLine("MORE");
+        }
+
+        public static void WriteNumberIsLess()
+        {
+            Console.WriteLine("LESS");
+        }
+
+        public static void WriteRules(int minNumber, int maxNumber)
+        {
+            Console.WriteLine(" Guess the number ({0}-{1}).",minNumber,maxNumber);
+        }
+
+        public static string ReadPlayerName()
+        {
+            WriteInputNameMessage();
+            return Console.ReadLine();
+        }
+
+        private static void WriteInputNameMessage()
+        {
+            Console.Write("NAME:");
+        }
+
+        public static void WriteSwearing(String userName, Random random)
+        {
+            Console.WriteLine(OATH[random.Next(0, OATH.Length)].Replace("%NAME", userName));
+        }
+
+        public static bool ReadYesNo()
+        {
+            for (; ; )
+            {
+                WriteInputMessage("y/n");
+                var input = Console.ReadLine().ToLower();
+                if (input.Equals("y"))
+                {
+                    return true;
+                }
+                else if (input.Equals("n"))
+                {
+                    return false;
+                }
+                else
+                {
+                    WriteInputError();
+                }
+            }
+        }
+
+        private static void WriteInputError()
+        {
+            Console.WriteLine("Invalid input. Try again.");
+        }
+
+        private static void WriteInputMessage(String options = "")
+        {
+            Console.Write("INPUT");
+            if (options != "")
+            {
+                Console.Write("({0})", options);
+            }
+            Console.Write(": ");
+        }
+
+        public static int ReadNumber()
+        {
+            int result = 0;
+            bool correctInput = false;
+            while (!correctInput)
+            {
+                WriteInputMessage();
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                    correctInput = true;
+                }
+                catch (Exception e)
+                {
+                    WriteInputError();
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Khenkina.Nsudotnet.NumberGuesser/Khenkina.Nsudotnet.NumberGuesser/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khenkina.Nsudotnet.NumberGuesser
+{
+    class Program
+    {
+        private const int MinNumber = 0;
+        private const int MaxNumber = 100;
+
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            String userName = GameIO.ReadPlayerName();
+
+            bool playAgain = true;
+            while (playAgain)
+            {
+                int hiddenNumber = random.Next(MinNumber, MaxNumber);
+                int attempts = 0;
+                GameIO.WriteRules(MinNumber, MaxNumber);
+
+                bool won = false;
+                DateTime startTime = DateTime.Now;
+                while (!won)
+                {
+                    int number = GameIO.ReadNumber();
+                    attempts++;
+
+                    if (number == hiddenNumber)
+                    {
+                        EndGame(attempts, DateTime.Now - startTime, ref playAgain);
+                        won = true;
+                    }
+                    else
+                    {
+                        if (number < hiddenNumber)
+                        {
+                            NumberIsMore();
+                        }
+                        else
+                        {
+                            NumberIsLess();
+                        }
+                        if (attempts % 4 == 0) GameIO.WriteSwearing(userName, random);
+                    }
+                }
+            }
+
+        }
+
+        private static void NumberIsLess()
+        {
+            GameIO.WriteNumberIsLess();
+        }
+
+        private static void NumberIsMore()
+        {
+            GameIO.WriteNumberIsMore();
+        }
+
+        private static void EndGame(int attempts, TimeSpan playedTime, ref bool playAgain)
+        {
+            GameIO.WriteResult(attempts, playedTime);
+            playAgain = GameIO.ReadYesNo();
+        }
+    }
+}
